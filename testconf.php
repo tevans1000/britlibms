889da39c93ce79c62dea7c18270aa979e351968e
Thomas Evans <tevans1000@googlemail.com>
2016-07-13 23:04:12 +0100

    tests database connection via external config file

--- testconf.php
+++ testconf.php
@@ -2,7 +2,23 @@
 
 require_once( '../async/conf.php' );
 
+/*
 $smarty = new Smarty();
 $smarty -> testInstall();
+*/
 
+$qstr = file_get_contents( '../async/test.sql' );
+$stmt = $db->prepare($qstr);
+$stmt->execute();
+$stmt -> setFetchMode(PDO::FETCH_ASSOC);
+$result = $stmt -> fetchAll();
 ?>
+<dl>
+    <?php
+    foreach ($result as $row){
+        foreach ($row as $k => $v){
+            echo('<dt>' . $k . '</dt><dd>' . $v . '</dd>');
+        }
+    }
+    ?>
+</dl>
