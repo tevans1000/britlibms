31b0c3363bf5ac481dd252736a6813ba38cf1723
root <root@britlib-manuscripts.cs.ucl.ac.uk>
2016-07-14 00:59:58 +0100

    cache file auto-update by Smarty

---
+++ templates_c/2dbf01d10aabe9d317684f233c3a73b6fbe3ba98_0.file.index.tpl.php
@@ -0,0 +1,28 @@
+<?php
+/* Smarty version 3.1.28, created on 2016-07-13 22:51:40
+  from "/var/www/html/britlibms/sync/templates/index.tpl" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.28',
+  'unifunc' => 'content_5786c5fc0831b6_71583954',
+  'file_dependency' => 
+  array (
+    '2dbf01d10aabe9d317684f233c3a73b6fbe3ba98' => 
+    array (
+      0 => '/var/www/html/britlibms/sync/templates/index.tpl',
+      1 => 1468450198,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5786c5fc0831b6_71583954 ($_smarty_tpl) {
+?>
+
+
+Hello <?php echo $_smarty_tpl->tpl_vars['name']->value;?>
+, welcome to Smarty!<?php }
+}
