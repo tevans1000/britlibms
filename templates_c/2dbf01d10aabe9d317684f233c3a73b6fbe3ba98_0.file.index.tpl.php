a42ad99f5ee52f603012a7db353ac5b7419f7209
root <root@britlib-manuscripts.cs.ucl.ac.uk>
2016-07-16 12:21:47 +0100

    smarty cache auto-update

--- templates_c/2dbf01d10aabe9d317684f233c3a73b6fbe3ba98_0.file.index.tpl.php
+++ templates_c/2dbf01d10aabe9d317684f233c3a73b6fbe3ba98_0.file.index.tpl.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.28, created on 2016-07-13 22:51:40
+/* Smarty version 3.1.28, created on 2016-07-16 12:19:00
   from "/var/www/html/britlibms/sync/templates/index.tpl" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.28',
-  'unifunc' => 'content_5786c5fc0831b6_71583954',
+  'unifunc' => 'content_578a1824e99162_43225978',
   'file_dependency' => 
   array (
     '2dbf01d10aabe9d317684f233c3a73b6fbe3ba98' => 
     array (
       0 => '/var/www/html/britlibms/sync/templates/index.tpl',
-      1 => 1468450198,
+      1 => 1468667848,
       2 => 'file',
     ),
   ),
@@ -19,10 +19,12 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
   array (
   ),
 ),false)) {
-function content_5786c5fc0831b6_71583954 ($_smarty_tpl) {
+function content_578a1824e99162_43225978 ($_smarty_tpl) {
 ?>
 
 
-Hello <?php echo $_smarty_tpl->tpl_vars['name']->value;?>
-, welcome to Smarty!<?php }
+You are on page <?php echo $_smarty_tpl->tpl_vars['pageno']->value;?>
+, viewing <?php echo $_smarty_tpl->tpl_vars['perpage']->value;?>
+ of the <?php echo $_smarty_tpl->tpl_vars['mscount']->value;?>
+ results.<?php }
 }
