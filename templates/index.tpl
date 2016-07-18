ffb2ce6ae9da76d864c610ff240700dba95b0711
Thomas Evans <tevans1000@googlemail.com>
2016-07-18 22:43:59 +0100

    seperate column for filters

--- templates/index.tpl
+++ templates/index.tpl
@@ -10,37 +10,92 @@
 </head>
 <body>
     <div class='container-fluid'>
-        <p>
-            You are on page {$pageno} of {$maxpage},
-            viewing {$perpage} of the {$mscount} results.
-        </p>
-        <ul>
-            {foreach $mslist as $ms}
-            <li>
-                <ul>
-                    {foreach $ms as $attr}
-                    <li>
-                        {$attr}
-                    </li>
-                    {/foreach}
-                </ul>
-            </li>
-            {/foreach}
-        </ul>
-        <ul class='pagination'>
-            {for $linkno=$pageno-2 to $pageno+2}
-            {if ($linkno<=0) || ($linkno>$maxpage)}
-            {continue}
-            {/if}
-            {if $linkno==$pageno}
-            <li class='active'>
-            {else}
-            <li>
-                {/if}
-                <a href='?page={$linkno}'>{$linkno}</a>
-            </li>
-            {/for}
-        </ul>
+        <div class='row'>
+            <div class='col-sm-12'>
+                <header>
+                    <h2>
+                        Header
+                    </h2>
+                </header>
+            </div>
+        </div>
+        <div class='row'>
+            <div class='col-sm-2'>
+                <nav>
+                    <h2>
+                        Filter tabs
+                    </h2>
+                    <ul>
+                        <li>
+                            Year
+                        </li>
+                        <li>
+                            Language
+                        </li>
+                        <li>
+                            Region
+                        </li>
+                        <li>
+                            etc.
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class='col-sm-10'>
+                <section>
+                    <h1>
+                        Results
+                    </h1>
+                    <p>
+                        You are on page {$pageno} of {$maxpage},
+                        viewing {$perpage} of the {$mscount} results.
+                    </p>
+                    <ul>
+                        {foreach $mslist as $ms}
+                        <li>
+                            <ul>
+                                {foreach $ms as $attr}
+                                <li>
+                                    {$attr}
+                                </li>
+                                {/foreach}
+                            </ul>
+                        </li>
+                        {/foreach}
+                    </ul> 
+                </section>
+            </div>
+        </div>
+        <div class='row'>
+            <div class='col-sm-12'>
+                <nav>
+                    <ul class='pagination'>
+                        {for $linkno=$pageno-2 to $pageno+2}
+                        {if ($linkno<=0) || ($linkno>$maxpage)}
+                        {continue}
+                        {/if}
+                        {if $linkno==$pageno}
+                        <li class='active'>
+                        {else}
+                        <li>
+                        {/if}
+                            <a href='?page={$linkno}'>{$linkno}</a>
+                        </li>
+                        {/for}
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class='row'>
+            <div class='col-sm-12'>
+                <footer>
+                    <h2>
+                        Footer
+                    </h2>
+                </footer>
+            </div>
+        </div>
+        
     </div>
 </body>
 </html>
--- templates_c/be13fdd9e2afcbfbdd3e6e268c97f7ceac6741ae_0.file.index.tpl.php
+++ templates_c/be13fdd9e2afcbfbdd3e6e268c97f7ceac6741ae_0.file.index.tpl.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.28, created on 2016-07-18 19:20:35
+/* Smarty version 3.1.28, created on 2016-07-18 22:42:32
   from "C:\wamp\www\britlibms\sync\templates\index.tpl" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.28',
-  'unifunc' => 'content_578d1df3903278_66468268',
+  'unifunc' => 'content_578d4d48b0c142_58838601',
   'file_dependency' => 
   array (
     'be13fdd9e2afcbfbdd3e6e268c97f7ceac6741ae' => 
     array (
       0 => 'C:\\wamp\\www\\britlibms\\sync\\templates\\index.tpl',
-      1 => 1468866033,
+      1 => 1468878148,
       2 => 'file',
     ),
   ),
@@ -19,7 +19,7 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
   array (
   ),
 ),false)) {
-function content_578d1df3903278_66468268 ($_smarty_tpl) {
+function content_578d4d48b0c142_58838601 ($_smarty_tpl) {
 ?>
 <!DOCTYPE html>
 <html lang='en'>
@@ -37,16 +37,52 @@ function content_578d1df3903278_66468268 ($_smarty_tpl) {
 </head>
 <body>
     <div class='container-fluid'>
-        <p>
-            You are on page <?php echo $_smarty_tpl->tpl_vars['pageno']->value;?>
+        <div class='row'>
+            <div class='col-sm-12'>
+                <header>
+                    <h2>
+                        Header
+                    </h2>
+                </header>
+            </div>
+        </div>
+        <div class='row'>
+            <div class='col-sm-2'>
+                <nav>
+                    <h2>
+                        Filter tabs
+                    </h2>
+                    <ul>
+                        <li>
+                            Year
+                        </li>
+                        <li>
+                            Language
+                        </li>
+                        <li>
+                            Region
+                        </li>
+                        <li>
+                            etc.
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class='col-sm-10'>
+                <section>
+                    <h1>
+                        Results
+                    </h1>
+                    <p>
+                        You are on page <?php echo $_smarty_tpl->tpl_vars['pageno']->value;?>
  of <?php echo $_smarty_tpl->tpl_vars['maxpage']->value;?>
 ,
-            viewing <?php echo $_smarty_tpl->tpl_vars['perpage']->value;?>
+                        viewing <?php echo $_smarty_tpl->tpl_vars['perpage']->value;?>
  of the <?php echo $_smarty_tpl->tpl_vars['mscount']->value;?>
  results.
-        </p>
-        <ul>
-            <?php
+                    </p>
+                    <ul>
+                        <?php
 $_from = $_smarty_tpl->tpl_vars['mslist']->value;
 if (!is_array($_from) && !is_object($_from)) {
 settype($_from, 'array');
@@ -58,9 +94,9 @@ if ($__foreach_ms_0_total) {
 foreach ($_from as $_smarty_tpl->tpl_vars['ms']->value) {
 $__foreach_ms_0_saved_local_item = $_smarty_tpl->tpl_vars['ms'];
 ?>
-            <li>
-                <ul>
-                    <?php
+                        <li>
+                            <ul>
+                                <?php
 $_from = $_smarty_tpl->tpl_vars['ms']->value;
 if (!is_array($_from) && !is_object($_from)) {
 settype($_from, 'array');
@@ -72,11 +108,11 @@ if ($__foreach_attr_1_total) {
 foreach ($_from as $_smarty_tpl->tpl_vars['attr']->value) {
 $__foreach_attr_1_saved_local_item = $_smarty_tpl->tpl_vars['attr'];
 ?>
-                    <li>
-                        <?php echo $_smarty_tpl->tpl_vars['attr']->value;?>
+                                <li>
+                                    <?php echo $_smarty_tpl->tpl_vars['attr']->value;?>
 
-                    </li>
-                    <?php
+                                </li>
+                                <?php
 $_smarty_tpl->tpl_vars['attr'] = $__foreach_attr_1_saved_local_item;
 }
 }
@@ -84,9 +120,9 @@ if ($__foreach_attr_1_saved_item) {
 $_smarty_tpl->tpl_vars['attr'] = $__foreach_attr_1_saved_item;
 }
 ?>
-                </ul>
-            </li>
-            <?php
+                            </ul>
+                        </li>
+                        <?php
 $_smarty_tpl->tpl_vars['ms'] = $__foreach_ms_0_saved_local_item;
 }
 }
@@ -94,30 +130,49 @@ if ($__foreach_ms_0_saved_item) {
 $_smarty_tpl->tpl_vars['ms'] = $__foreach_ms_0_saved_item;
 }
 ?>
-        </ul>
-        <ul class='pagination'>
-            <?php
+                    </ul> 
+                </section>
+            </div>
+        </div>
+        <div class='row'>
+            <div class='col-sm-12'>
+                <nav>
+                    <ul class='pagination'>
+                        <?php
 $_smarty_tpl->tpl_vars['linkno'] = new Smarty_Variable;$_smarty_tpl->tpl_vars['linkno']->step = 1;$_smarty_tpl->tpl_vars['linkno']->total = (int) ceil(($_smarty_tpl->tpl_vars['linkno']->step > 0 ? $_smarty_tpl->tpl_vars['pageno']->value+2+1 - ($_smarty_tpl->tpl_vars['pageno']->value-2) : $_smarty_tpl->tpl_vars['pageno']->value-2-($_smarty_tpl->tpl_vars['pageno']->value+2)+1)/abs($_smarty_tpl->tpl_vars['linkno']->step));
 if ($_smarty_tpl->tpl_vars['linkno']->total > 0) {
 for ($_smarty_tpl->tpl_vars['linkno']->value = $_smarty_tpl->tpl_vars['pageno']->value-2, $_smarty_tpl->tpl_vars['linkno']->iteration = 1;$_smarty_tpl->tpl_vars['linkno']->iteration <= $_smarty_tpl->tpl_vars['linkno']->total;$_smarty_tpl->tpl_vars['linkno']->value += $_smarty_tpl->tpl_vars['linkno']->step, $_smarty_tpl->tpl_vars['linkno']->iteration++) {
 $_smarty_tpl->tpl_vars['linkno']->first = $_smarty_tpl->tpl_vars['linkno']->iteration == 1;$_smarty_tpl->tpl_vars['linkno']->last = $_smarty_tpl->tpl_vars['linkno']->iteration == $_smarty_tpl->tpl_vars['linkno']->total;?>
-            <?php if (($_smarty_tpl->tpl_vars['linkno']->value <= 0) || ($_smarty_tpl->tpl_vars['linkno']->value > $_smarty_tpl->tpl_vars['maxpage']->value)) {?>
-            <?php continue 1;?>
-            <?php }?>
-            <?php if ($_smarty_tpl->tpl_vars['linkno']->value == $_smarty_tpl->tpl_vars['pageno']->value) {?>
-            <li class='active'>
-            <?php } else { ?>
-            <li>
-                <?php }?>
-                <a href='?page=<?php echo $_smarty_tpl->tpl_vars['linkno']->value;?>
+                        <?php if (($_smarty_tpl->tpl_vars['linkno']->value <= 0) || ($_smarty_tpl->tpl_vars['linkno']->value > $_smarty_tpl->tpl_vars['maxpage']->value)) {?>
+                        <?php continue 1;?>
+                        <?php }?>
+                        <?php if ($_smarty_tpl->tpl_vars['linkno']->value == $_smarty_tpl->tpl_vars['pageno']->value) {?>
+                        <li class='active'>
+                        <?php } else { ?>
+                        <li>
+                        <?php }?>
+                            <a href='?page=<?php echo $_smarty_tpl->tpl_vars['linkno']->value;?>
 '><?php echo $_smarty_tpl->tpl_vars['linkno']->value;?>
 </a>
-            </li>
-            <?php }
+                        </li>
+                        <?php }
 }
 ?>
 
-        </ul>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class='row'>
+            <div class='col-sm-12'>
+                <footer>
+                    <h2>
+                        Footer
+                    </h2>
+                </footer>
+            </div>
+        </div>
+        
     </div>
 </body>
 </html>
